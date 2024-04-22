c49aab1afe75a0012e9e480639cbad3807bbcae2
Amath <amathndaoseck@gmail.com>
2024-04-22 20:09:08 +0200

    📦 NEW:

--- Testamath.py
+++ Testamath.py
@@ -1,3 +1,5 @@
+import unittest
+
 def greater_number(x, y, z):
     # Comparer les trois nombres pour trouver le plus grand
     greater_number = x
@@ -6,10 +8,20 @@ def greater_number(x, y, z):
     if z > greater_number:
         greater_number = z
     return greater_number
-# Demander à l'utilisateur d'entrer trois nombres
-x = float(input("Entrez le premier nombre : "))
-y = float(input("Entrez le deuxième nombre : "))
-z = float(input("Entrez le troisième nombre : "))
-# Appeler la fonction pour trouver le plus grand nombre
-result = greater_number(x, y, z)
-print("Le plus grand nombre est :", result)
+
+class TestGreaterNumber(unittest.TestCase):
+
+    def test_positive_numbers(self):
+        self.assertEqual(greater_number(1, 2, 3), 3)
+
+    def test_negative_numbers(self):
+        self.assertEqual(greater_number(-1, -2, -3), -1)
+
+    def test_mixed_numbers(self):
+        self.assertEqual(greater_number(-1, 2, 0), 2)
+
+    def test_equal_numbers(self):
+        self.assertEqual(greater_number(3, 3, 3), 3)
+
+if __name__ == '__main__':
+    unittest.main()
