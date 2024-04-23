864ed05c8254fbad1956d0b2481d2dc89412ed69
SSalim <salim.bouhidel@ecoles-epsi.net>
2024-04-23 10:23:32 +0200

    🐛 FIX: correction test

--- Exotestsalim.py
+++ Exotestsalim.py
@@ -1,13 +1,14 @@
-
-
 #importer unitest
 import unittest
 
 # Importer la fonction à tester depuis le fichier ou le code se trouve
 from Exosalim import reverseOrdre
 
+# définir une class test
+class testFonction(unittest.TestCase) : 
+
 # Définir les cas de test
-def test_reverseOrdre(self):
+    def test_reverseOrdre(self):
         # Cas de test avec une liste non vide
         self.assertEqual(reverseOrdre([1, 2, 3]), [3, 2, 1])
         # Cas de test avec une liste vide
@@ -19,8 +20,8 @@ def test_reverseOrdre(self):
         # Cas de test avec une liste contenant des éléments dupliqués
         self.assertEqual(reverseOrdre([1, 2, 2, 3]), [3, 2, 2, 1])
         # Cas de test ou une partie des elemnets de la liste sont identiques
-        self.assertEqual(reverseOrdre([1, 1, a, b, 1], [1, b, a, 1, 1]))
-        
+        self.assertEqual(reverseOrdre([1, 1, 'a', 'b', 1], [1, 'b', 'a', 1, 1])
+     
 # Exécuter les tests
 if __name__ == '__main__':
     unittest.main()
